1f8347d82144b819fbd3be2f45aca24c40985191
Gabryss <zeratul312@gmail.com>
2024-06-19 12:07:14 +0200

    ISM update

--- slam/slam_toolbox_config/odometry_ws/build/rf2o_laser_odometry/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/spacer/slam/slam_toolbox_config/odometry_ws/src
-# Build directory: /home/spacer/slam/slam_toolbox_config/odometry_ws/build/rf2o_laser_odometry
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
